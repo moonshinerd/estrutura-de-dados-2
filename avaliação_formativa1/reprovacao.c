ed1ee20cc5d5cd59584ed63c6d2100f35ec0bd54
moonshinerd <vitorhls15@gmail.com>
2024-10-19 17:27:54 -0300

    add . adicionando implementação inicial do reprovacao.c

---
+++ avaliação_formativa1/reprovacao.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h> 
+#include <ctype.h>
+
+int armazana_notas_semestre(){
+
+}
+
+int main() {
+    int total_disciplinas;
+    scanf("%d", &total_disciplinas);
+    int notas_por_disciplia[total_disciplinas];
+
+
+}
